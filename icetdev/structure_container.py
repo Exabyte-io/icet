02caeba0461277b6dd91f128165873a5a4a19793
williamz <william.munoz@chalmers.se>
2017-11-16 22:11:35 +0100

    MAINT: mv structure_container.py and its unit test (edited) to icet-dev/ and test/

--- icetdev/structure_container.py
+++ icetdev/structure_container.py
@@ -1,21 +1,22 @@
-from collections import OrderedDict
-
 
 class StructureContainer(object):
 
-    def __init__(self, clusterspace, atoms_list=None):
-        '''
-        Initializes a StructureContainer object.
+    def __init__(self, clusterspace, 
+                 atoms_list=None, 
+                 properties=None):
 
-        This class serves as a container for ASE Atoms objects, their fit
-        properties and their cluster vectors.
+        '''
+        Initializes a StructureContainer object
+    
+        This class serves as a container for ase-atoms objects, their fit
+        properties and their cluster vectors. 
 
         Attributes:
         -----------
         clusterspace : icet ClusterSpace object
             the cluster space used for evaluating the cluster vectors
-
-        atoms_list : list of ASE-Atoms objects
+            
+        atoms_list : list of ASE Atoms objects
             input structures
         '''
 
@@ -26,72 +27,46 @@ class StructureContainer(object):
             if not isinstance(atoms_list, list):
                 raise ValueError('atoms_list must be list or None.')
             self._structure_list = []
-            for atoms in atoms_list:
-                self.add_structure(atoms)
+            for atoms, properties in zip(atoms_list, properties):
+                self.add_structure(atoms=atoms, properties=properties)
 
     def __len__(self):
         return len(self._structure_list)
 
-    def __getitem__(self, index):
-        return self._structure_list[index]
+    def __getitem__(self, ind):
+        return self._structure_list[ind]
 
-    def get_number_of_structures(self):
-        '''
-        Return the total number of structures in the container.
-        '''
-        return len(self._structure_list)
-
-    def get_structure(self, user_tag):
-        '''
-        Return the structure in the form of an ASE Atoms object.
+    def get_structure_indices(self, user_tag=None):
+        ''' 
+        Get structure indices via user_tag
 
         Parameters
         ----------
         user_tag : str
-            tag assigned during structure addition
-        '''
-        for structure in self._structure_list:
-            if structure.user_tag == user_tag:
-                return structure.atoms
+            user_tag used for selecting structures
 
-    def get_clustervector(self, user_tag):
+        Returns
+        -------
+        list
+            List of structures with corresponding user_tag
         '''
-        Return cluster vector of structure in container.
-
-        Parameters
-        ----------
-        user_tag : str
-            tag assigned during structure addition
-        '''
-        for structure in self._structure_list:
-            if structure.user_tag == user_tag:
-                return structure.cvs
-
-    def get_properties(self, key, user_tag):
-        '''
-        Return a list of avaliable properties
-
-        Parameters
-        ----------
-        key : str
-            name the desired property listed in user keys of structures
-        user_tag : str
-            custom user tag to identify strcuture
-        '''
-        for structure in self._structure_list:
-            if structure.user_tag == user_tag:
-                return structure.properties[key]
+        return [i for i, s in enumerate(self) if user_tag is None or s.user_tag == user_tag]
 
     def __repr__(self):
-        '''
-        Print basic information about each structure in structure list
+        ''' 
+        Print basic information about each structure in structure list 
+        
+        TODO: print properties (with default units) in more fancy way
         '''
         def repr_structure(index, structure):
+            from collections import OrderedDict
             fields = OrderedDict([
-                ('index',      '{:4}'.format(index)),
-                ('user_tag',   '{:24}'.format(structure.user_tag)),
-                ('numatoms',   '{:5}'.format(len(structure))),
-                ('properties', '{:}'.format(structure.properties))])
+                ('index',     '{:4}'.format(index)),
+                ('user_tag',  '{:12}'.format(structure.user_tag)),
+                ('properties', '{:}'.format('   '.join(['{:}:{:9.6f}'.format(k ,v)
+                                                       for k, v in structure.properties.items()]))),
+                ('numatoms',  '{:5}'.format(len(structure))),
+                ('fit_ready', '{:5}'.format(str(structure.fit_ready)))])
             s = []
             for name, value in fields.items():
                 n = max(len(name), len(value))
@@ -103,15 +78,30 @@ class StructureContainer(object):
 
         dummy = self._structure_list[0]
         n = len(repr_structure(-1, dummy))
+        horizontal_line = '{s:-^{n}}'.format(s='', n=n)
+
         s = []
         s += ['{s:-^{n}}'.format(s=' Structure Container ', n=n)]
+        s += ['Total number of structures: {}'.format(len(self))]
         s += [repr_structure(-1, dummy)]
-        for i, structure in enumerate(self._structure_list):
-            s += [repr_structure(i, structure)]
-        return '\n'.join(s)
+        # table body
+        index = 0
+        print_threshold = 24
+        while index < len(self):
+            if (len(self) > print_threshold
+                    and index > 10 and index < len(self) - 10):
+                index = len(self) - 10
+                s += [' ...']
+
+            s += [repr_structure(index, self._structure_list[index])]
+            index += 1
+
+        return '\n'.join(s)           
+
 
     def add_structure(self, atoms, user_tag=None,
-                      compute_cvs=True,
+                      properties =  None,
+                      compute_cvs=False,
                       map_to_prototype=False):
         '''
         Add a structure to the training set.
@@ -123,23 +113,116 @@ class StructureContainer(object):
         user_tag : str
             custom user tag to identify structure
         compute_clustervector: bool
-            if True clustervector is computed
-        '''
+            if True, clustervector is computed
 
+        '''
+        import logging
         atoms_copy = atoms.copy()
-        atoms_copy.keys = atoms.keys
-        structure = FitStructure(atoms_copy, user_tag=atoms.tag)
+
+        if user_tag is None:
+            user_tag = atoms_copy.get_chemical_formula(mode='hill')
+
+        structure = FitStructure(atoms_copy, user_tag)
+
+        if properties is not None:
+            structure.set_properties(properties)            
+        elif atoms.calc is not None:
+            structure.set_properties({'energy': atoms.get_potential_energy()/len(atoms)}) 
+        else:
+            logging.warning('Skipping atoms object, no properties found')
+            return
 
         if compute_cvs:
-            cvs = self.clusterspace.get_clustervector(structure.atoms)
-            structure.set_cvs(cvs)
+            cv = self.clusterspace.get_clustervector(atoms_copy)
+            structure.set_cvs(cv)
 
         self._structure_list.append(structure)
 
+
+    def load_properties(self, structures=None, properties=None):
+        '''
+        This method allows you to add properties and/or modify 
+        the values of existing properties
+
+        Parameters
+        ----------
+        structures: list, tuple, int
+            list of indices of the desired structures
+
+        properties: dict
+            properties to add
+        
+        '''
+        if properties is None:
+            raise ValueError('load_properties() requires at least properties in arguments')
+        
+        if structures is None:
+            for s, prop in zip(self._structure_list, properties):
+                s.set_properties(prop)
+        else:
+            for i, prop in zip(structures, properties):
+                if not self._structure_list[i].fit_ready:
+                    raise ValueError('Structure {} is not fit ready'.format(i))
+                self._structure_list[i].set_properties(prop)
+
+
+    def get_properties(self, structures=None, key='energy'):
+        '''
+        Return a list of properties of a list
+        of structures.
+
+        Parameters
+        ----------
+        structures: list, tuple, int
+            list of indices of the desired structures
+
+        key : string
+            key label of property
+
+
+        '''
+        if structures is None:
+            p_list = [s.properties[key]
+                      for s in self._structure_list] #if fit_ready
+        else:
+            p_list = []
+            for i in structures:
+                if not self._structure_list[i].fit_ready:
+                    raise ValueError('Structure {} is not fit ready'.format(i))
+                p_list.append(self._structure_list[i].properties[key])
+
+        return p_list
+
+
+    def get_structures(self, structures=None):
+        '''
+        Return a list of structures in the form of ASE Atoms
+        
+        Parameters
+        ----------
+        structures: list, tuple, int
+            list of indices of the desired structures
+
+        '''
+        if structures is None:
+            s_list = [s.atoms
+                      for s in self._structure_list]
+        else:
+            s_list = []
+            for i in structures:
+                if not self._structure_list[i].fit_ready:
+                    raise ValueError('Structure {} is not fit ready'.format(i))
+                s_list.append(self._structure_list[i].atoms)
+
+        return s_list
+
     @property
-    def clusterspace(self):
-        '''icet clusterspace object'''
-        return self._clusterspace
+    def get_cluster_space(self):
+        ''' 
+        Returns a copy of the icet ClusterSpace object. 
+        '''
+        return self._clusterspace #.copy()
+
 
 
 class FitStructure:
@@ -154,15 +237,16 @@ class FitStructure:
     user_tag : str
         custom user tag
     cvs : list of floats
-        clustervector
+        calculated clustervector for actual structure
     '''
 
-    def __init__(self, atoms, user_tag=None, cvs=None):
-        assert isinstance(user_tag, str) or user_tag is None, \
-            'user_tag must be a string'
+    def __init__(self, atoms, user_tag, cvs=None, properties=None):
         self._atoms = atoms
         self._user_tag = user_tag
+        self._fit_ready = False
+        self._properties = {}
         self.set_cvs(cvs)
+        self.set_properties(properties)
 
     @property
     def cvs(self):
@@ -175,26 +259,48 @@ class FitStructure:
         return self._atoms
 
     @property
-    def properties(self):
-        '''numpy array : property'''
-        return self._atoms.keys
-
-    @property
     def user_tag(self):
         return str(self._user_tag)
 
+    @property
+    def properties(self):
+        '''dict : properties'''
+        return self._properties
+
+    @property
+    def fit_ready(self):
+        '''boolean : True if the structure is prepared for fitting, i.e. the
+        clustervector is available'''
+        return self._fit_ready
+
+
     def set_cvs(self, cvs):
-        '''Set the clustervectors to structure.
+        '''
+        Set the clustervectors to structure.
 
         Parameters
         ----------
-        cvs : list of float
+        cvs : list of float 
             clustervector for this structure
+
         '''
         if cvs is not None:
             self._cvs = cvs
         else:
             self._cvs = None
+
+    def set_properties(self, properties):
+        '''
+        Set properties to structure.
+
+        Parameters
+        __________
+        properties: dict (key,value)
+            list of properties for this structures
+
+        '''
+        if properties is not None:
+            self._properties.update(properties)
 
     def __len__(self):
         return len(self._atoms)
--- tests/test_structure_container.py
+++ tests/test_structure_container.py
@@ -1,67 +1,177 @@
+#!/usr/bin/env python3
+
 '''
-This test checks that a structure container object can be initialized
-with a list of random structures given as ASE-Atoms objects along with
-properties and label tags
+This file contains unit tests and other tests. It can be executed by
+simply executing this file from a shell prompt:
+    
+    $ ./test_structure_container.py
+    
+In which case it will use the system's default python version. If a specific
+python version should be used, run that python version with this file as input,
+e.g.:
+    
+    python3 test_structure_container.py
+
+For a description of the python unit testing framework, see this link:
+https://docs.python.org/3/library/unittest.html
+
+When executing this file doc testing is also performed on all doc tests in 
+the structure_container.py file
+
 '''
+
+import unittest
 
 from icetdev import ClusterSpace, StructureContainer
 from ase.build import bulk
+from ase.calculators.emt import EMT
 from ase import Atoms
-import numpy as np
-
 
 subelements = ['Ag', 'Au']
 cutoffs = [4.0] * 3
 atoms_prim = bulk('Ag')
-
 atoms_supercell = atoms_prim.repeat(2)
 
+cs = ClusterSpace(atoms_supercell, cutoffs, subelements)
 
-def generate_configuration(atoms_supercell):
-    '''
-    Generate a structure for testing.
-    '''
+atoms_list=[]
 
-    from ase.calculators.emt import EMT
-    conf = atoms_supercell.copy()
+# structure #1
+conf_1 = atoms_supercell.copy()
+atoms_list.append(conf_1)
 
-    calc = EMT()
+# structure #2
+conf_2 = atoms_supercell.copy()
+conf_2[0].symbol = 'Au'
+atoms_list.append(conf_2)
+
+# structure #3
+conf_3 = atoms_supercell.copy()
+conf_3[0].symbol = 'Au'
+conf_3[1].symbol = 'Au'
+atoms_list.append(conf_3)
+
+calc = EMT()
+properties = []
+
+for conf in atoms_list:
     conf.set_calculator(calc)
-    conf.get_total_energy()
-    conf.keys = {'energy': conf.get_total_energy(),
-                 'efermi': 0.1,
-                 'LUMO': 3.0}
+    conf.properties = {'energy':conf.get_potential_energy()/len(conf), 
+                       'volumen':conf.get_volume()}
+    properties.append(conf.properties)
 
-    return conf
+class TestStructureContainer(unittest.TestCase):
+    '''
+    Container for tests of the class functionality
+    '''
+    
+    def setUp(self):
+        '''
+        Instantiate class before each test.
+
+        '''
+        self.sc = StructureContainer(cs, atoms_list, properties)
+    
+    def test_init(self):
+        '''
+        Just testing that the setup 
+        (initialization) of tested class work
+
+        '''
+        self.sc = StructureContainer(cs, atoms_list, properties)
+
+    def test_len(self):
+        '''
+        Testing len functionality
+
+        '''
+        number_structures = self.sc.__len__()
+        self.assertEqual(number_structures, len(atoms_list))
+
+    def test_getitem(self):
+        '''
+        Testing getitem functionality
+
+        '''
+        structure = self.sc.__getitem__(1)
+        self.assertIsNotNone(structure)
+
+    def test_get_structure_indices(self):
+        '''
+        Testing get_structure_indices functionality
+
+        '''
+        list_index = [x for x in range(len(atoms_list))]
+        self.assertEqual(self.sc.get_structure_indices(), list_index)
+
+    def test_add_structure(self):
+        '''
+        Testing add_structure functionality
+
+        '''
+        #structure #4
+        conf_4 = atoms_supercell.copy()
+        conf_4[0].symbol = 'Au'
+        conf_4[1].symbol = 'Au'
+        conf_4[2].symbol = 'Au'
+        conf_4.set_calculator(calc)
+        
+        self.sc.add_structure(conf_4)
+        
+        list_index = [x for x in range(len(atoms_list)+1)]
+        new_indices = self.sc.get_structure_indices()
+        self.assertEqual(new_indices,list_index)
+
+    def test_repr(self):
+        '''
+        Testing repr functionality
+
+        '''
+        retval = self.sc.__repr__()
+        target = """------------------------------- Structure Container -------------------------------
+Total number of structures: 3
+index |   user_tag   |              properties               | numatoms | fit_ready
+   0  | Ag8          | energy: 0.001584   volumen:136.835858 |      8   |   False  
+   1  | Ag7Au        | energy:-0.000915   volumen:136.835858 |      8   |   False  
+   2  | Ag6Au2       | energy:-0.003188   volumen:136.835858 |      8   |   False  """
+        print('Printing repr\n{}'.format(retval))
+        self.assertEqual(target, retval)
+
+    def test_get_properties(self):
+        '''
+        Testing get_properties functionality
+        
+        '''
+        p_list = self.sc.get_properties()
+        self.assertTrue(isinstance(properties,float) for properties in p_list)
+
+    def test_load_properties(self):
+        '''
+        Testing load_properties functionality
+
+        '''
+        extra_properties =[]
+        for conf in atoms_list:
+            extra_properties.append({'total_energy': conf.get_total_energy()})
+
+        self.sc.load_properties(properties=extra_properties)
+        p_list = self.sc.get_properties(key='total_energy')
+        self.assertTrue(isinstance(properties,float) for properties in p_list)
+
+    def test_get_structures(self):
+        '''
+        Testing get_strcutures functionality
+        '''
+        s_list = self.sc.get_structures()
+        self.assertTrue(isinstance(atoms, Atoms) for atoms in s_list)
+
+    def test_get_cluster_space(self):
+        '''
+        Testing get_clusterspace functionality
+        '''
+        sc_cs = self.sc.get_cluster_space
+        self.assertEqual(sc_cs, cs)
 
 
-print('')
-atoms_list = []
-number_of_structures = 4
-
-for index in range(number_of_structures):
-    conf = generate_configuration(atoms_supercell)
-    tag = 'user-provided-name-{}'.format(index)
-    conf.tag = tag
-    atoms_list.append(conf)
-
-cs = ClusterSpace(atoms_prim, cutoffs, subelements)
-sc = StructureContainer(cs, atoms_list)
-
-# testing total number of structures
-assert sc.get_number_of_structures() == number_of_structures
-
-# testing structure getter
-assert isinstance(sc.get_structure(conf.tag), Atoms)
-
-# testing clustervector
-cv_target = np.array(cs.get_clustervector(conf))
-cv = sc.get_clustervector(conf.tag)
-assert np.all(np.abs(cv_target - cv) < 1e-6)
-
-# testing list of available properties
-en = sc.get_properties('energy', conf.tag)
-assert en == conf.keys['energy']
-
-# test __repr__ function of StructureContainer class
-print(sc)
+if __name__ == '__main__':
+    unittest.main()
