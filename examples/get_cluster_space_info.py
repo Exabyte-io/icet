2f2db321c978439f99c68dba31cc0f4eb28eccd9
yoobro <joabro@chalmers.se>
2017-12-15 19:11:22 +0100

    ENH: Added updated versions of the example scripts.

--- examples/get_cluster_space_info.py
+++ examples/get_cluster_space_info.py
@@ -1,22 +1,35 @@
 '''
-This scripts shows how to otbain basic information about a cluster space.
+This examples demonstrates how to obtain basic information about a cluster
+space.
 '''
 
+# Start import
 from icetdev import ClusterSpace
 from icetdev.cluster_space import get_singlet_info, view_singlets
 from ase.build import bulk
+# End import
 
-cutoffs = [10.0, 7.0, 5.0]
-subelements = ['Re', 'Ti', 'W', 'Mo']
+# Create a prototype structure, decide which additional elements to populate
+# it with (Re, Ti, W and Mo) and set the cutoffs for pairs (10.0 A),
+# triplets (7.0 A) and quadruplets (5.0 A).
+# Start setup
 prototype = bulk('Re')
+subelements = ['Re', 'Ti', 'W', 'Mo']
+cutoffs = [10.0, 7.0, 5.0]
+# End setup
 
+# Generate and print the cluster space.
+# Start clusterspace
 clusterspace = ClusterSpace(prototype, cutoffs, subelements)
 print(clusterspace)
+# End clusterspace
 
+# Extract and print additional information regarding the singlets.
+# Start singlets
 print('\nSinglets:')
 cluster_data = get_singlet_info(prototype)
 for singlet in cluster_data:
     for key in singlet.keys():
         print(' {:22} : {}'.format(key, singlet[key]))
-
 view_singlets(prototype)
+# End singlets
--- examples/get_native_clusters.py
+++ examples/get_native_clusters.py
@@ -1,30 +1,39 @@
-from icetdev.clusterspace import create_clusterspace
+'''
+This examples demonstrates how to find the native clusters for a structure
+'''
+
+# Start import
+from icetdev.cluster_space import ClusterSpace
 from icetdev.cluster_counts import ClusterCounts
-from icetdev.structure import structure_from_atoms
+from icetdev.structure import Structure
 from ase.build import bulk
 from ase import Atoms
 import numpy as np
+# End import
 
-
-
-
-def print_native_clusters(clustercounts, structure):
-    print("Native cluster counts for:")
-    print(structure)
-    clustercounts.print()
-    
-
+# Create a prototype structure, decide which additional elements to populate
+# it with (Si, Ge) and set the cutoff for pairs (10.0 A)
+# Start setup
 conf = bulk("Si")
 cutoffs = [10.0]
 subelements = ["Si", "Ge"]
+# End setup
 
-clusterspace = create_clusterspace(conf, cutoffs, subelements)
+# Generate the cluster space.
+# Start clusterspace
+clusterspace = ClusterSpace(conf, cutoffs, subelements)
+# End clusterspace
 
-
+# Prepare 2x2x1 supercells, populate these, randomly, with Si and Ge atoms,
+# then extract and print the native clusters.
+# Start native
 for i in range(1):
-    supercell = bulk("Si").repeat([2,2,1])
+    supercell = bulk("Si").repeat([2, 2, 1])
     for atom in supercell:
         atom.symbol = np.random.choice(subelements)
-    supercell = structure_from_atoms(supercell)        
-    clustercounts = clusterspace.get_native_clusters(supercell)
-    print_native_clusters(clustercounts, supercell)
+    structure = Structure.from_atoms(supercell)
+    nativeclusters = clusterspace.get_native_clusters(structure)
+    print("Native cluster counts for:")
+    print(structure)
+    nativeclusters.print()
+# End native
