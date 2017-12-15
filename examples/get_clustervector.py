fef092fd0ad3f79c8bb00d4560a9ac025f4dae6b
yoobro <joabro@chalmers.se>
2017-12-15 19:11:22 +0100

    ENH: Added updated versions of the example scripts.

--- examples/get_clustervector.py
+++ examples/get_clustervector.py
@@ -1,30 +1,40 @@
-# Step 1
-from icetdev.clusterspace import create_clusterspace
+'''
+This examples demonstrates how to construct cluster vectors.
+'''
+
+# Start import
+from icetdev.cluster_space import ClusterSpace
 from ase.build import bulk
 from ase import Atoms
+# End import
 
-
+# Create a prototype structure, decide which additional elements to populate
+# it with (Si, Ge) and set the cutoffs for pairs (5.0 A), triplets (5.0 A)
+# and quadruplets (5.0 A).
+# Start setup
 conf = bulk("Si")
 cutoffs = [5.0, 5.0, 5.0]
 subelements = ["Si", "Ge"]
+# End setup
 
-clusterspace = create_clusterspace(conf, cutoffs, subelements)
-# Step 1.1
+# Generate and print the cluster space.
+# Start clusterspace
+clusterspace = ClusterSpace(conf, cutoffs, subelements)
 print(clusterspace)
+# End clusterspace
 
-# Step 2
 
+# Generate and print the cluster vector for a pure Si 2x2x2 supercell.
+# Start clustervector1
 supercell = bulk("Si").repeat(2)
-
 cv = clusterspace.get_clustervector(supercell)
 print(cv)
+# End clustervector1
 
-# Step 3
-
+# Generate and print the cluster vector for a mixed Si-Ge 2x2x2 supercell
+# Start clustervector2
 supercell_2 = bulk("Si").repeat(2)
 supercell_2[0].symbol = "Ge"
-
 cv_2 = clusterspace.get_clustervector(supercell_2)
-
 print(cv_2)
-
+# End clustervector2
--- examples/test_cell_geometry.py
+++ examples/test_cell_geometry.py
@@ -1,52 +1,59 @@
-from icetdev.tools.geometry import get_scaled_positions, find_lattice_site_from_position_python
+'''
+This examples demonstrates how to find the lattice sites for a structure from
+the list of nearest neighbors
+'''
 
-from icetdev.structure import *
-from icetdev.neighborlist import *
-from ase import Atoms, Atom
+# Start import
+from icetdev.tools.geometry import get_scaled_positions
+from icetdev.structure import Structure
+from icetdev.neighborlist import Neighborlist
+from ase import Atom
 from ase.build import bulk
-
+import time
 import numpy as np
+# End import
 
-
-# ASE atoms
+# Create a prototype Al structure in the form of a 1x1x1 unit cell with 2
+# extra Al atoms.
+# Start setup
 atoms = bulk("Al", "fcc", a=2.0).repeat(1)
-atoms.append(Atom("Al",position=[0.5,0.5,0.5]))
-atoms.append(Atom("Al",position=[0.25,0.5,0.5]))
+atoms.append(Atom("Al", position=[0.5, 0.5, 0.5]))
+atoms.append(Atom("Al", position=[0.25, 0.5, 0.5]))
+structure = Structure().from_atoms(atoms)
+# End setup
 
-#get neighborlist
-neighbor_cutoff = 6.0 #Ångström
-structure = structure_from_atoms(atoms)
+# Construct a list of all neighbors within the cutoff (6.0 A).
+# Start neighbor
+neighbor_cutoff = 6.0
 nl = Neighborlist(neighbor_cutoff)
 nl.build(structure)
-pos_neighbors = []
+# End neighbor
 
-#get frac positions
+# Extract the positions and fractional coordinates for all neighbors.
+# Start position
+pos_neighbors = []
 for latNbr in nl.get_neighbors(0):
     pos = structure.get_position(latNbr)
     pos_neighbors.append(pos)
-frac_coordinates = get_scaled_positions(np.array(pos_neighbors), cell=atoms.cell, wrap=False, pbc = structure.pbc)
+frac_coordinates = get_scaled_positions(np.array(pos_neighbors),
+                                        cell=atoms.cell, wrap=False,
+                                        pbc=structure.pbc)
+# End position
 
-import time
-
+# Check the time required for finding all the lattice sites for the positions
+# of the neighbors
+# Start lattice_sites
 t0 = time.time()
-#print to see what happens
-print("#1: frac pos #2: position #3: fpos*cell")
-lat_nbrs = structure.findLatticeSitesFromPositions(pos_neighbors)
-
-# for fpos, pos in zip(frac_coordinates,pos_neighbors):
-#     lat_nbr = structure.findLatticeSiteFromPosition(pos)
-#     #lat_nbr = find_lattice_site_from_position_python(structure, pos)
-#     #lat_pos = structure.positions[lat_nbr.index] + np.dot(lat_nbr.unitcellOffset, structure.cell)
-#     #print(fpos, pos, np.dot(fpos, atoms.cell), lat_nbr, lat_pos-pos )
-
-# for fpos, pos, lat_nbr in zip(frac_coordinates,pos_neighbors, lat_nbrs):
-#     #lat_nbr = structure.findLatticeSiteFromPosition(pos)
-#     #lat_nbr = find_lattice_site_from_position_python(structure, pos)
-#     lat_pos = structure.positions[lat_nbr.index] + np.dot(lat_nbr.unitcellOffset, structure.cell)
-#     print(fpos, pos, np.dot(fpos, atoms.cell), lat_nbr, lat_pos-pos )
-
-
-
+lat_nbrs = structure.find_lattice_sites_by_positions(pos_neighbors)
 t1 = time.time()
-print("time for finding latnbrs: {} ms ".format( round((t0-t1)*1e3,4)))
-print("Found {} LatticeSites".format(len(lat_nbrs)))
+print("# Found {} LatticeSites in {} ms ".format(len(lat_nbrs),
+                                                 round((t0-t1)*1e3, 4)))
+# End lattice_sites
+
+# Print the fractional coordinates, positions, lattice sites for all
+# neihbors.
+# Start results
+print("#1: frac. pos. #2: position #3: latt. site")
+for fpos, pos, lat_nbr in zip(frac_coordinates, pos_neighbors, lat_nbrs):
+    print(fpos, pos, lat_nbr)
+# End results
