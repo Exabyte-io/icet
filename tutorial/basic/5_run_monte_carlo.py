b5ce079f34bd5687fc6c3c4208ac0d729f5112b3
williamz <william.munoz@chalmers.se>
2018-11-29 10:45:30 +0100

    STY: Fixed flake8 error

--- tutorial/basic/5_run_monte_carlo.py
+++ tutorial/basic/5_run_monte_carlo.py
@@ -10,8 +10,8 @@ ce = ClusterExpansion.read('mixing_energy.ce')
 chemical_symbols = ce.cluster_space.chemical_symbols[0]
 atoms = make_supercell(ce.cluster_space.primitive_structure,
                        3 * array([[-1, 1, 1],
-                                [1, -1, 1],
-                                [1, 1, -1]]))
+                                 [1, -1, 1],
+                                 [1, 1, -1]]))
 # TODO: Remove this line once cs.primitive cell is not longer decorated
 # with H atoms
 atoms.numbers = [47]*len(atoms)
