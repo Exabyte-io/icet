3eab2cbe6519a8e1f607ea83f8a2667c97272703
Erik Fransson <karl.erik.fransson@gmail.com>
2019-02-14 20:55:59 +0100

    initial commit, should work

--- mchammer/ensembles/base_ensemble.py
+++ mchammer/ensembles/base_ensemble.py
@@ -111,7 +111,7 @@ class BaseEnsemble(ABC):
             self._data_container = DataContainer.read(data_container)
 
             dc_ensemble_parameters = self.data_container.ensemble_parameters
-            if self.ensemble_parameters != dc_ensemble_parameters:
+            if not dicts_equals(self.ensemble_parameters, dc_ensemble_parameters):
                 raise ValueError('Ensemble parameters do not match with those'
                                  ' stored in DataContainer file: {}'.format(
                                      set(dc_ensemble_parameters.items()) -
@@ -495,3 +495,20 @@ class BaseEnsemble(ABC):
     def ensemble_parameters(self) -> dict:
         """Returns parameters associated with the ensemble."""
         return self._ensemble_parameters.copy()
+
+
+def dicts_equals(dict1, dict2, atol=1e-12):
+    """ Check if two dicts are equal, if float or intergers are in the dicts
+    then atol is used for comparing them. """
+    if len(dict1) != len(dict2):
+        return False
+    for key in dict1.keys():
+        if key not in dict2:
+            return False
+        if isinstance(dict1[key], (int, float)) and isinstance(dict2[key], (int, float)):
+            if not np.allclose(dict1[key], dict2[key], rtol=0.0, atol=atol):
+                return False
+        else:
+            if dict1[key] != dict2[key]:
+                return False
+    return True
