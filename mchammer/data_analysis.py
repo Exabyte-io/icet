f91d1b8174018236fff573d8e0cffb61bd7a34b2
Erik Fransson <karl.erik.fransson@gmail.com>
2019-02-15 09:50:57 +0100

    initial data analysis

---
+++ mchammer/data_analysis.py
@@ -0,0 +1,70 @@
+import pandas as pd
+import numpy as np
+import scipy
+
+
+def compute_autocorrelation_function(data, max_lag=None):
+    """ Estimates autocorrelation function.
+
+    The autocorrelation function is computed using Pandas.Series.autocorr
+
+    Parameters
+    ----------
+    data : np.ndarray
+        data series to compute autocorrelation function for
+    max_lag : int
+        maximum lag between two data points
+
+    Returns
+    -------
+    numpy.ndarray
+        calculated autocorrelation function
+    """
+    if max_lag is None:
+        max_lag = len(data) - 1
+    if 1 > max_lag >= len(data):
+        raise ValueError('max_lag should be between 1 and len(data)-1.')
+    series = pd.Series(data)
+    acf = [series.autocorr(lag) for lag in range(0, max_lag)]
+    return np.array(acf)
+
+
+def estimate_correlation_length(data):
+    """ Estimate the correlation length of data.
+
+    The correlation length is taken as the first point where the
+    autocorrelation functions is less than exp(-2).
+
+    If correlation function never goes below exp(-2) then np.nan is returned
+
+    Parameters
+    ----------
+    data : np.ndarray
+        data series to compute autocorrelation function for
+
+    Returns
+    -------
+    numpy.ndarray
+        calculated autocorrelation function
+    """
+
+    acf = compute_autocorrelation_function(data)
+    lengths = np.where(acf < np.exp(-2))[0]  # ACF < exp(-2)
+    if len(lengths) == 0:
+        return np.nan
+    else:
+        return lengths[0]
+
+
+def estimate_error(data, confidence=0.95):
+    """ Standard error estimate with confidence interval.
+
+    error = t_factor * std(data) / sqrt(Ns)
+    where t_factor is the factor corresponding to the confidence interval
+    Ns is the number of indepdent measurements
+
+    """
+    corr_length = estimate_correlation_length(data)
+    t_factor = scipy.stats.t.ppf((1 + confidence) / 2, len(data)-1)
+    error = t_factor * np.std(data) / np.sqrt(len(data)/corr_length)
+    return error
