f295b6ff264a4e46d3031c651eea03eeb4382467
Erik Fransson <karl.erik.fransson@gmail.com>
2019-03-12 22:20:38 +0100

    CA in ensemble init

--- mchammer/ensembles/__init__.py
+++ mchammer/ensembles/__init__.py
@@ -3,7 +3,9 @@
 from .canonical_ensemble import CanonicalEnsemble
 from .semi_grand_canonical_ensemble import SemiGrandCanonicalEnsemble
 from .vcsgc_ensemble import VCSGCEnsemble
+from .canonical_annealing import CanonicalAnnealing
 
 __all__ = ['CanonicalEnsemble',
            'SemiGrandCanonicalEnsemble',
-           'VCSGCEnsemble']
+           'VCSGCEnsemble',
+           'CanonicalAnnealing']
