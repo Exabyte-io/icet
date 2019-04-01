9c4e3d9a70384a186befc32ea483d0744c38a145
Mattias Ångqvist <angqvist@chalmers.se>
2019-04-01 12:35:53 +0000

    Update icet/core/sublattices.py

--- icet/core/sublattices.py
+++ icet/core/sublattices.py
@@ -78,7 +78,6 @@ class Sublattices:
         self._sublattices = []
         sublattice_to_indices = [[] for _ in range(len(self._allowed_species))]
         for index, position in enumerate(structure.get_positions()):
-
             lattice_site = cpp_prim_structure.find_lattice_site_by_position(
                 position)
 
