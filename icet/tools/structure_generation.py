4dbaa369855c4fc201826cea23ba9c92f5da7c04
Magnus Rahm <rmagnus@chalmers.se>
2019-04-06 17:07:59 +0200

    ENH: Seemingly fully functioning SQS vector calculator

--- icet/tools/structure_generation.py
+++ icet/tools/structure_generation.py
@@ -3,6 +3,7 @@ from mchammer.calculators.target_vector_calculator import TargetVectorCalculator
 from icet.tools import enumerate_supercells
 from icet import ClusterSpace
 from ase.build import bulk
+from ase.data import chemical_symbols as periodic_table
 import numpy as np
 import random
 from typing import List
@@ -73,12 +74,15 @@ def decorate_atoms_randomly(atoms: Atoms, target_concentrations: dict):
     # symbols_all will hold chemical_symbols of all sublattices
     symbols_all = [0] * len(atoms)
     for sublattice in cs.get_sublattices(atoms):
+        print(len(sublattice.indices))
         symbols = []  # chemical_symbols in one sublattice
         for chemical_symbol in sublattice.chemical_symbols:
+            print(chemical_symbol)
             n_symbol = int(
                 round(n_atoms * target_concentrations[chemical_symbol]))
             symbols += [chemical_symbol] * n_symbol
 
+        print(len(symbols))
         # If any concentration was not commensurate with the sublattice,
         # raise an exception
         if len(symbols) != len(sublattice.indices):
@@ -140,23 +144,26 @@ def get_random_vector(cluster_space: ClusterSpace,
     return cv
 
 
-def tmp(cluster_space, target_concentrations):
+def get_sqs_cluster_vector(cluster_space, target_concentrations):
     sublattice_to_index = {letter: index for index,
                            letter in enumerate('ABCDEFGHIJKLMNOPQ')}
     all_sublattices = cluster_space.get_sublattices(cs.primitive_structure)
 
     # Make a map from chemical symbol to integer, later on used
     # for evaluating cluster functions.
-    # NB: This will probably not match icet. What is the icet convention?
+    # Internally, icet sorts species according to atomic numbers.
     # Also check that each symbol only occurs in one sublattice.
     symbol_to_integer_map = {}
-    found_symbols = []
+    found_species = []
     for sublattice in all_sublattices:
-        for i, symbol in enumerate(sublattice.chemical_symbols):
-            if symbol in found_symbols:
+        syms = sublattice.chemical_symbols
+        atomic_numbers = [periodic_table.index(sym) for sym in syms]
+        for i, species in enumerate(sorted(atomic_numbers)):
+            if species in found_species:
                 raise ValueError('Each chemical symbol can only occur on one '
-                                 'sublattice, {} occurred more than once.'.format(symbol))
-            symbol_to_integer_map[symbol] = i
+                                 'sublattice, {} occurred more than once.'.format(periodic_table[species]))
+            found_species.append(species)
+            symbol_to_integer_map[periodic_table[species]] = i
 
     # Target concentrations refer to all atoms, but probabilities only
     # to the sublattice.
@@ -168,9 +175,9 @@ def tmp(cluster_space, target_concentrations):
 
 
     # For every orbit, calculate average cluster function
-    cv = []
+    cv = [1.0]
     for orbit in cluster_space.orbit_data:
-        if orbit['order'] < 2:
+        if orbit['order'] < 1:
             continue
 
         # What sublattices are there in this orbit?
@@ -186,7 +193,7 @@ def tmp(cluster_space, target_concentrations):
                                   for symbol_group in symbol_groups]
 
         # Calculate contribtion from every possible combination of symbols
-        # weighted with their concentration
+        # weighted with their probability
         cluster_product_average = 0
         count = 0
         for symbols in itertools.product(*symbol_groups):
@@ -207,20 +214,22 @@ if __name__ == '__main__':
     atoms = bulk('Au', a=4.0)
     atoms.append(Atom('H', position=(2, 2, 2)))
 
+    
+    
     #atoms.append(Atom('H', position=(2, 2, 2)))
-    cs = ClusterSpace(atoms, [6.0], [['Au', 'Pd'], ['H', 'V']])
+    cs = ClusterSpace(atoms, [6.0], [['Au', 'Pd', 'Cu'], ['H', 'V']])
 
     maxsize = 10
-    target_concentrations = {'Au': 3 / 10,
-                             'Pd': 2 / 10,
-                             'H': 4/10,
-                             'V': 1/10,
+    target_concentrations = {'Au': 0.6666666667 / 6,
+                             'Pd': 1.333333333 / 6,
+                             'Cu': 1 / 6,
+                             'H': 2.5/6,
+                             'V': 0.5/6,
                              }  # 'H': 1 / 4, 'V': 1 / 4}
-    cv = tmp(cs, target_concentrations)
+    cv = get_sqs_cluster_vector(cs, target_concentrations)
     print(cv)
-    cv_random = get_random_vector(cs, target_concentrations)
-    cv_random = cv_random[len(cv_random) - len(cv):]
-    cv_diff = np.abs(cv) - np.abs(cv_random)
+    cv_random = get_random_vector(cs, target_concentrations, supercell_size=2000)
+    cv_diff = cv - cv_random
 
     print(cv_random)
     print(cv)
